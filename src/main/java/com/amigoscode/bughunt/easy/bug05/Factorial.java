d0845111d3ea235e1eccd055c7970ad722b8edae
Franco Polizzi <me@fpolizzi.com>
2026-05-21 22:41:42 +0200

    fix: solve bug05

--- src/main/java/com/amigoscode/bughunt/easy/bug05/Factorial.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug05/Factorial.java
@@ -3,7 +3,7 @@ package com.amigoscode.bughunt.easy.bug05;
 public class Factorial {
 
     public long factorial(int n) {
-        int result = 1;
+        long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
