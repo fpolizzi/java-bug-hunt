39c169ce1b7fa865c0bd9db616678a80e80db81c
Franco Polizzi <me@fpolizzi.com>
2026-05-22 23:16:22 +0200

    fix: solve bug06

--- src/main/java/com/amigoscode/bughunt/easy/bug06/Average.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug06/Average.java
@@ -3,6 +3,6 @@ package com.amigoscode.bughunt.easy.bug06;
 public class Average {
 
     public double average(int a, int b, int c) {
-        return a + b + c / 3;
+        return (a + b + c) / 3;
     }
 }
