2d6bc44d35e58e088c8a155f778b4272f392eb01
Franco Polizzi <me@fpolizzi.com>
2026-05-23 23:22:50 +0200

    fix: solve bug07

--- src/main/java/com/amigoscode/bughunt/easy/bug07/Reverser.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug07/Reverser.java
@@ -4,7 +4,7 @@ public class Reverser {
 
     public String reverse(String input) {
         StringBuilder sb = new StringBuilder();
-        for (int i = input.length(); i >= 0; i--) {
+        for (int i = input.length() - 1; i >= 0; i--) {
             sb.append(input.charAt(i));
         }
         return sb.toString();
