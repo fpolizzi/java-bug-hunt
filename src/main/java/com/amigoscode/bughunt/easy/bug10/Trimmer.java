813e2664d59c4397465949efc721034811c50a56
Franco Polizzi <me@fpolizzi.com>
2026-05-29 23:38:38 +0200

    fix: solve bug10

--- src/main/java/com/amigoscode/bughunt/easy/bug10/Trimmer.java
+++ src/main/java/com/amigoscode/bughunt/easy/bug10/Trimmer.java
@@ -3,8 +3,7 @@ package com.amigoscode.bughunt.easy.bug10;
 public class Trimmer {
 
     public String cleanup(String input) {
-        input.trim();
-        input.toLowerCase();
-        return input;
+
+        return input.trim().toLowerCase();
     }
 }
